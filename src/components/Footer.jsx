8aff6eb4622608ac804709ecfc1c527adf3f03a0
Junaid <44660674+jahmad7@users.noreply.github.com>
2023-09-28 12:25:04 -0700

    fixed incorrect link:

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -22,7 +22,7 @@ export function Footer() {
             <div className="flex flex-col items-center justify-between gap-6 sm:flex-row">
               <div className="flex flex-wrap justify-center gap-x-6 gap-y-1 text-sm font-medium text-zinc-800 dark:text-zinc-200">
                 <NavLink href="/about">About</NavLink>
-                <NavLink href="/ai-junaid">AI Me</NavLink>
+                <NavLink href="/ai">AI Me</NavLink>
                 <NavLink href="/education">Education</NavLink>
                 <NavLink href="/experiences">Experiences</NavLink>
               </div>
